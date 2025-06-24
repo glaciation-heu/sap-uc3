c504cc0ca034f564909301a76a3ade7fc3b7904f
Nicolai Huening <nicolai.huening@sap.com>
2025-06-24 11:40:51 +0200

    Update logging

--- coordination_service/src/notification_service.rs
+++ coordination_service/src/notification_service.rs
@@ -5,6 +5,7 @@ use tracing::{event, Level};
 use crate::{cs_execute::ExecutionResult, error::Result};
 
 pub async fn notify_parties(output_parties: Vec<String>, result: ExecutionResult) -> Result<()> {
+    event!(Level::INFO, "Notifying output parties {:?}", output_parties);
     for party in output_parties {
         let response = Client::new()
             .put(format!("{}/notify", party))
@@ -13,11 +14,11 @@ pub async fn notify_parties(output_parties: Vec<String>, result: ExecutionResult
             .header("Content-Type","application/json")
             .send().await?;
             if response.status().is_success() {
-                event!(Level::INFO,"Output party {} was notified.", party)
+                event!(Level::INFO,"Output party {} was notified.", party);
             } else {
                 let body = response.bytes().await?.to_vec();
                 let s = String::from_utf8_lossy(&body);
-                event!(Level::WARN,"Unable to notify output party {}", s)
+                event!(Level::WARN,"Unable to notify output party {}", s);
             }
     }
     Ok(())
